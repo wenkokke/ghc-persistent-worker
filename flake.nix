2f3d5f65b9dc880a58ce0a76ec92998fc798d6bf
Torsten Schmits <git@tryp.io>
2025-04-16 12:45:02 +0200

    add hix build
    Change-Id: If2e0f47b5842f15443dcd0ab58f05c2a206980c2

--- flake.nix
+++ flake.nix
@@ -1,53 +1,67 @@
 {
-  description = "A flake for ghc-persistent-worker";
+  description = "GHC persistent worker";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    hix.url = "github:tek/hix";
+    hix.inputs.nixpkgs.url = "github:nixos/nixpkgs/b2243f41e860ac85c0b446eadc6930359b294e79";
+    ghc-debug = {
+      url = "git+https://gitlab.haskell.org/ghc/ghc-debug";
+      flake = false;
+    };
+    fenix = {
+      url = "github:nix-community/fenix/6c51b42ac2c25328067956ff980572482786d20c";
+      inputs.nixpkgs.url = "github:nixos/nixpkgs/9807714d6944a957c2e036f84b0ff8caf9930bc0";
+    };
+    nix.url = "github:nixos/nix/2.34.2";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+  outputs = inputs@{hix, ...}: hix [({config, lib, util, ...}: {
+    ghcVersions = ["ghc914"];
+    main = "ghc-worker";
+    ghci.args = ["-package ghc"];
+    hls.genCabal = false;
 
-      in
-      {
-        devShells.default =
-          let
-            libs = [
-              pkgs.zstd
-              pkgs.xz
-              pkgs.bzip2
-              pkgs.zlib
-            ];
-          in
-          pkgs.mkShell {
-            ghc_dir = "${pkgs.haskell.compiler.ghc910}";
+    compilers = {
 
-            packages = [
-              (pkgs.haskell.packages.ghc910.ghcWithPackages (g: [ g.cabal-install ]))
-              pkgs.zlib.dev
-              pkgs.snappy
-              pkgs.protobuf
-              pkgs.git
-              pkgs.nixfmt-rfc-style
-            ] ++ libs;
+      # Roughly the GHC used by MWB.
+      mwb-26-04.source.build = {
+        url = "https://gitlab.haskell.org/ghc/ghc";
+        version = "9.10.1";
+        flavour = "release+split_sections+ipe";
+        rev = "fe5932d418e8221bb5bcd34954caca07ff8d9310";
+        hash = "sha256-4ZhFSs6ZC3XXmrcmZ72ca0sn+RZVNgx5rbiwZuRJigY=";
+      };
 
-            shellHook = ''
-              # prompt
-              export PS1="\n[ghc-persistent-worker:\w]$ \0"
-              # dynamic library path
-              export LD_LIBRARY_PATH="${pkgs.lib.makeSearchPathOutput "out" "lib" libs}:$LD_LIBRARY_PATH"
-            '';
-          };
-      }
-    );
+      # Some as `mwb-26-04`, but with fixed nodes.
+      mwb-26-04-fixed.source.build = {
+        url = "https://gitlab.haskell.org/ghc/ghc";
+        version = "9.10.1";
+        flavour = "release+split_sections+ipe";
+        rev = "630ee987758fe2fce24113afd445fa95e4b82503";
+        hash = "sha256-frUOGkwnYbCJooJtMYVpdGEWmi05rKjP6Dd2h0zq/8Q=";
+      };
+
+      ghc914.nixpkgs = "ghc914";
+
+    };
+
+    nixpkgs = {
+
+      ghc914.source = {
+        rev = "c6d65881c5624c9cae5ea6cedef24699b0c0a4c0";
+        hash = "sha256-WNGcmeOZ8Tr9dq6ztCspYbzWFswr2mPebM9LpsfGxPk=";
+      };
+
+    };
+
+    internal.hixCli.dev = true;
+
+  })
+
+  (import ./ops/packages.nix)
+  (import ./ops/tools.nix)
+  (import ./ops/package-sets.nix inputs)
+
+  ];
+
 }
