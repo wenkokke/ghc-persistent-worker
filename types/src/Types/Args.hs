e23fb2ce8c981aad27aa4030a4d5a6d93ab36c1d
Torsten Schmits <git@tryp.io>
2026-06-05 18:41:11 +0200

    Read the env var `buck_source_hashes` when parsing args
    Change-Id: Ie004ee376d6478cc81fc985a4636fe5ece06aba8

--- types/src/Types/Args.hs
+++ types/src/Types/Args.hs
@@ -11,6 +11,7 @@ import GHC.Paths (libdir)
 import GHC.Unit (UnitId)
 import GHC.Utils.Outputable (showPprUnsafe)
 import System.OsPath.Extra (OsPath, toOsPath)
+import Types.BuildPlan.Incremental (BuckHashesPath)
 import Types.CachedDeps (CachedBuildPlans, CachedDeps)
 import Types.FeatureFlags (FeatureFlags, defaultFeatureFlags)
 import Types.Target (ModuleTarget)
@@ -92,6 +93,8 @@ data Args =
     tempDir :: Maybe OsPath,
     unit :: Maybe UnitName,
     buildPlan :: Maybe OsPath,
+    -- | The file containing the current source hashes tracked by the external build tool.
+    sourceHashes :: Maybe BuckHashesPath,
     -- | The build plan fields included in the JSON.
     fields :: Maybe (NonEmpty BuildPlanField),
     moduleTarget :: Maybe ModuleTarget,
@@ -113,6 +116,7 @@ emptyArgs env =
     tempDir = toOsPath <$> (env !? "TMPDIR"),
     unit = Nothing,
     buildPlan = Nothing,
+    sourceHashes = Nothing,
     fields = Nothing,
     moduleTarget = Nothing,
     ghcOptions = [],
